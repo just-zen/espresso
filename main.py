e90f55485ccb93d1129e8e149248081576d65238
justzen <justzenn@mail.ru>
2022-11-25 17:40:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from cof import Ui_MainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+        self.con = sqlite3.connect("coffee.db")
+        cur = self.con.cursor()
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        cur = self.con.cursor()
+        if self.lineEdit.text():
+            result = cur.execute(f'{self.lineEdit.text()}').fetchall()
+        else:
+            result = cur.execute('SELECT * FROM variants').fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
